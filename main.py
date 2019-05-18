0af2feb44677fa759e7eb60fc2aa97d306b2ac27
Jenny Duckett <jenny.duckett@digital.justice.gov.uk>
2019-05-18 18:31:59 +0100

    Get started by reading data from a spreadsheet
    This is from the quickstart guide [1], minimally adapted to read the
    spreadsheet id and cell range from envvars.
    
    [1]: https://developers.google.com/sheets/api/quickstart/python

---
+++ main.py
@@ -0,0 +1,56 @@
+import pickle
+import os
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
+RANGE_NAME = os.environ['RANGE_NAME']
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    """
+    creds = None
+
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        for row in values:
+            print(row)
+
+
+if __name__ == '__main__':
+    main()
